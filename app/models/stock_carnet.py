8887fa78ce966513bc8c453f46df2fc87746045c
CB PAUL <sony.paul2015@gmail.com>
2026-03-28 18:46:02 +0300

    fix pydantic v2 types

--- app/models/stock_carnet.py
+++ app/models/stock_carnet.py
@@ -8,7 +8,5 @@ class Stock_Carnet(SQLModel, table=True):
     quantite_disponible: int = Field(default=0)
     numero_serie_debut: Optional[str] = None
     numero_serie_fin: Optional[str] = None
-    taille_carnet: Optional[int] = Field(default=50)
-    statut: Optional[str] = Field(default="disponible")
-
-
+    taille_carnet: int = Field(default=50)   # ✅ corrigé ici
+    statut: str = Field(default="disponible")  # ✅ pas Optional
